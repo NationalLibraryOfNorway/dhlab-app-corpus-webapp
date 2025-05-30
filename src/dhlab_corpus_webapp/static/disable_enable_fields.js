3a4c4a809e3f52acca30a8563577b53ca1abaf49
Thea Tollersrud <thea.tollersrud@nb.no>
2025-05-30 16:25:31 +0200

    endret defaultverdien når annen doctype enn digibok velges

--- src/dhlab_corpus_webapp/static/disable_enable_fields.js
+++ src/dhlab_corpus_webapp/static/disable_enable_fields.js
@@ -1,24 +1,31 @@
 document.addEventListener("click", function() {
     var docTypeSelection = document.getElementById('doc_type_selection_');
-    if (!docTypeSelection) {
-        console.error("Dropdown not found");
-        return; 
-    }
+    //if (!docTypeSelection) {
+    //    console.error("Dropdown not found");
+    //    return; 
+    //}
     
     var authorField = document.getElementById('author_');
     var deweyField = document.getElementById('dewey_')
+
+    var authorOriginalPlaceholder = authorField.placeholder || "Henrik Ibsen";
+    var deweyOriginalPlaceholder = deweyField.placeholder || "";
 
     function updateAuthorField() {
         console.log("Dropdown value changed to:", docTypeSelection.value); 
 
         if (docTypeSelection.value === 'digibok') {
             console.log("Enabling author field for Digibok");
-            authorField.disabled =false;
-            deweyField.disabled=false;
+            authorField.readOnly =false; //endret fra "disabled" for å unngå rødt stoppskilt
+            deweyField.readOnly=false;
+            authorField.placeholder = authorOriginalPlaceholder;
+            deweyField.placeholder = deweyOriginalPlaceholder;
 
         } else {
-            authorField.disabled=true;
-            deweyField.disabled=true;
+            authorField.readOnly=true;
+            deweyField.readOnly=true;
+            authorField.placeholder = "Kan ikke velges";
+            deweyField.placeholder = "Kan ikke velges";
 
         }
     }
