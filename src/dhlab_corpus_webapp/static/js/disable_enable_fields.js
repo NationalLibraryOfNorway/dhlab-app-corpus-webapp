00b11ca804fef71b304b6b624eeb0c421b713d29
Magnus Breder Birkenes <magnus.birkenes@nb.no>
2025-11-03 10:12:48 +0100

    legg til emnefelt, skru alltid på innholdsord

--- src/dhlab_corpus_webapp/static/js/disable_enable_fields.js
+++ src/dhlab_corpus_webapp/static/js/disable_enable_fields.js
@@ -9,34 +9,30 @@ function autoupdateAllowedCorpusSearchFields() {
     let docTypeSelection = document.getElementById('doc-type-selection');
     function updateAllowedFields() {
         let authorField = document.getElementById('author-input');
+        let subjectField = document.getElementById('subject-input');
         let deweyField = document.getElementById('dewey-input')
         let wordsPhrasesField = document.getElementById('words-or-phrases-input')
 
         if (docTypeSelection.value === 'digibok') {
             authorField.disabled = false;
             deweyField.disabled = false;
+            subjectField.disabled = false;
 
         } else {
             authorField.disabled = true;
             authorField.value = "";
             deweyField.disabled = true;
             deweyField.value = "";
+            subjectField.disabled = true;
+            subjectField.value = "";
         }
 
-        if (docTypeSelection.value == 'nettavis') {
-            wordsPhrasesField.disabled = true;
-            wordsPhrasesField.value = "";
-        } else {
-            wordsPhrasesField.disabled = false;
-        }
         if (docTypeSelection.value == 'digavis') {
-            wordsPhrasesField.disabled = true;
             if (languageSelector){
                 languageSelector.selectedItems.map((value) => languageSelector.unselect(value.value))
                 languageSelector.disable();
             }
         } else {
-            wordsPhrasesField.disabled = false;
             if (languageSelector)
                 languageSelector.enable();
         }
